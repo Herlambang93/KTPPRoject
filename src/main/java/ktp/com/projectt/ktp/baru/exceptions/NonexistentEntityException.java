f8286be20f2297aad61ee7a87c8c2bbfbbfef6a7
Herlambang <muhammadherlambang9b@gmail.com>
2022-05-20 23:43:22 +0700

    first commit

---
+++ src/main/java/ktp/com/projectt/ktp/baru/exceptions/NonexistentEntityException.java
@@ -0,0 +1,10 @@
+package ktp.com.projectt.ktp.baru.exceptions;
+
+public class NonexistentEntityException extends Exception {
+    public NonexistentEntityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public NonexistentEntityException(String message) {
+        super(message);
+    }
+}
---
+++ src/test/java/ktp/com/projectt/ktp/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ktp.com.projectt.ktp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
